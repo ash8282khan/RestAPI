33e418af630695674eec5192bdce9d34d8bf7f0f
ash8282khan <ash8282khan@gmail.com>
2024-12-02 16:08:01 +0530

    SpringBoot Rest Api Development

---
+++ src/main/java/edu/kiet/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package edu.kiet.controller;
+
+import edu.kiet.model.Employee;
+import edu.kiet.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        service.deleteEmployee(id);
+    }
+}
---
+++ src/main/java/edu/kiet/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package edu.kiet.service;
+
+import edu.kiet.model.Employee;
+import edu.kiet.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
+    public List<Employee> getAllEmployees() {
+        return repository.findAll();
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return repository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        repository.deleteById(id);
+    }
+}
